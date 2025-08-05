d39b8846d232d3df12704aaf532eb26e3c3d0d81
Pushpraj31 <87000556+Pushpraj31@users.noreply.github.com>
2025-08-05 23:00:45 +0530

    login page

--- client/src/components/common/LoginPage.jsx
+++ client/src/components/common/LoginPage.jsx
@@ -1,7 +1,47 @@
 
+import { useState } from "react";
+import FormSection from "./forms/FormSection";
+import FormHeader from "./forms/FormHeader";
+import loginHeader from "../../assets/loginHeader.jpg";
+import FormFooter from "./forms/FormFooter";
+
 const LoginPage = () => {
+const [title, setTitle] = useState("Welcome Back Gorgeous 👋");
+  const [description, setDescription] = useState(
+    "Sign in to book your next glam session , track appointments , and unlock sweet beauty perks. Your glow-up is just a tap away!"
+  );
+
+  const headerData = [{ loginHeader: loginHeader, alt: "Login Header" }];
+
+  const inputData = [
+    {
+      labelName: "Email/Mobile No.",
+      type: "text",
+      id: "Email/Mobile No.",
+      placeholder: "Enter Your email or mobile number",
+    },
+    {
+      labelName: "password",
+      type: "password",
+      id: "password",
+      placeholder: "At least 8 characters",
+    },
+  ];
+
   return (
-    <div>LoginPage</div>
+    <div className="flex justify-center ">
+      <div className="w-96 flex flex-col  justify-between gap-3 p-4 rounded-md shadow-2xl">
+        <FormHeader headerData={headerData} />
+
+        <FormSection
+          title={title}
+          description={description}
+          inputData={inputData}
+        />
+        <FormFooter />
+        
+      </div>
+    </div>
   )
 }
 
