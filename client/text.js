f00617c0d19838b8e5319bc19d13c4a4c385aa5b
Aadarsh <aadarsh0811@gmail.com>
2026-01-28 16:05:57 +0530

    make the server deployment ready

--- client/text.js
+++
@@ -1,5 +0,0 @@
-{"web":
-    {"client_id":"1041132655349-c5cdp50ut4a6o39sl9rqjsaaeid55lbk.apps.googleusercontent.com","project_id":"avian-altar-485611-j9","auth_uri":"https://accounts.google.com/o/oauth2/auth","token_uri":"https://oauth2.googleapis.com/token",
-    "auth_provider_x509_cert_url":"https://www.googleapis.com/oauth2/v1/certs","client_secret":"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",
-    "javascript_origins":["https://wemakeover.co.in"]}
-}
