83a2647ed983ef35e96600009998f6cc6ef5ab66
Aadarsh <aadarsh0811@gmail.com>
2026-02-22 13:24:27 +0530

    fixed merge conflict in addressSlice.js

--- client/src/features/address/addressSlice.js
+++ client/src/features/address/addressSlice.js
@@ -1,8 +1,5 @@
 import { createSlice } from '@reduxjs/toolkit';
-<<<<<<< HEAD
 import { logoutUser } from '../auth/authThunks';
-=======
->>>>>>> 2e2ce50b2159a868378619e63443519cc5886ae8
 import {
   getUserAddresses,
   createAddress,
@@ -67,7 +64,6 @@ const addressSlice = createSlice({
   },
   extraReducers: (builder) => {
     builder
-<<<<<<< HEAD
       // Clear address state on logout
       .addCase(logoutUser.fulfilled, (state) => {
         state.addresses = [];
@@ -76,8 +72,6 @@ const addressSlice = createSlice({
         state.error = null;
         state.lastUpdated = null;
       })
-=======
->>>>>>> 2e2ce50b2159a868378619e63443519cc5886ae8
       // Get User Addresses
       .addCase(getUserAddresses.pending, (state) => {
         state.isLoading = true;
